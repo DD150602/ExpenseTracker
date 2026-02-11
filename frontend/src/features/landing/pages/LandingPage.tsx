44d0b6ac24db4ed48bb2bcd168219f0e347d793c
DD150602 <dadrdev@gmail.com>
2026-02-10 19:24:38 -0500

    arreglo import de componente

--- frontend/src/features/landing/pages/LandingPage.tsx
+++ frontend/src/features/landing/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
 import { CTA } from '@/features/landing/ui/Cta'
 import { Footer } from '@/features/landing/ui/Footer'
-import { Features } from '@/features/landing/ui/features'
+import { Features } from '@/features/landing/ui/Features'
 import { Header } from '@/features/landing/ui/Header'
 import { Hero } from '@/features/landing/ui/Hero'
 import { HowItWorks } from '@/features/landing/ui/HowItWorks'
